dc6fca280d8fdd2990e1290c62dcfedfe62e8e6a
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2022-02-27 16:29:57 +0900

    Complete Singleton Pattern

---
+++ Singleton/index.js
@@ -0,0 +1,22 @@
+var Singleton = /** @class */ (function () {
+    function Singleton() {
+    }
+    Singleton.getInstance = function () {
+        if (!Singleton.instance) {
+            Singleton.instance = new Singleton();
+        }
+        return Singleton.instance;
+    };
+    return Singleton;
+}());
+var clientCode = function () {
+    var s1 = Singleton.getInstance();
+    var s2 = Singleton.getInstance();
+    if (s1 === s2) {
+        console.log('Singleton works, both variables contain the same instance.');
+    }
+    else {
+        console.log('Singleton failed, variables contain different instances.');
+    }
+};
+clientCode();
